be30a89f0d0f400879fc4e5496e19e58b84e39d0
Kevin <kevin@erdee.de>
2021-10-22 16:39:02 +0200

    remove defect tagmanager

--- custom/apps/ShopStudioGoogleTagManagerCloud/Resources/app/storefront/src/main.js
+++
@@ -1,9 +0,0 @@
-import ShopStudioGoogleTagManagerCloud from './plugin/shop-studio-google-tag-manager-cloud/shop-studio-google-tag-manager-cloud.plugin';
-
-const { PluginManager } = window;
-
-PluginManager.register('ShopStudioGoogleTagManagerCloud', ShopStudioGoogleTagManagerCloud, '[data-shop-studio-google-tag-manager-cloud]');
-
-if (module.hot) {
-    module.hot.accept();
-}
